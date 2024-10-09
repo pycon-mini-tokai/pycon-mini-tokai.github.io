b0fadc82df4d315c14acdff85288c06e3f012ba1
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2024-10-09 20:37:13 +0900

    Permalink for each talk and workshop

--- 2024/index.js
+++ 2024/index.js
@@ -6,12 +6,27 @@ document.getElementById("event-modal-close").addEventListener("click", () => {
   for (const talk of document.querySelectorAll(".event-modal-talk")) {
     talk.style.display = "none";
   }
+  history.replaceState({}, "", "#");
 });
 
 for (const eventBox of document.querySelectorAll(".event-box")) {
   eventBox.addEventListener("click", (e) => {
-    const id = `detail_${e.currentTarget.id}`;
+    e.preventDefault();
+    const targetElementId = e.currentTarget.getAttribute("href").substring(1);
+    history.replaceState({}, "", `#${targetElementId}`);
     modalBackground.style.display = "block";
-    document.getElementById(id).style.display = "block";
+    document.getElementById(targetElementId).style.display = "block";
   });
+}
+
+// Open event modal if it's specified in the URL when DOM is ready
+if (window.location.hash.length > 1) {
+  const candidateId = window.location.hash.substring(1);
+  const candidateElement = document.getElementById(candidateId);
+  if (candidateElement && candidateElement.classList.contains("event-modal-talk")) {
+      document.getElementById("timetable").scrollIntoView();
+      modalBackground.style.display = "block";
+      candidateElement.style.display = "block";
+      candidateElement.scrollTop = 0;
+  }
 }
