bd8b4e444a79c6e8b64a13649325a57f89809606
fukicycle <lego.sato.4135@gmail.com>
2025-07-25 13:17:31 +0900

    Move all content files.

---
+++ 2025/index.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const targets = document.querySelectorAll('.slidein');
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  targets.forEach(target => {
+    observer.observe(target);
+  });
+});
