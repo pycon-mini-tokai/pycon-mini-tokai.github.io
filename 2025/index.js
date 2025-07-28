a75d94d955994d53b599e3966f3c9249e1e01079
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2025-07-28 21:49:21 +0900

    Run Prettier

--- 2025/index.js
+++ 2025/index.js
@@ -1,18 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const targets = document.querySelectorAll('.scroll-animation');
+  const targets = document.querySelectorAll(".scroll-animation");
 
-  const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
-        observer.unobserve(entry.target);
-      }
-    });
-  }, {
-    threshold: 0.1
-  });
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+    },
+  );
 
-  targets.forEach(target => {
+  targets.forEach((target) => {
     observer.observe(target);
   });
 });
